316d772f3cd674fd31674e9ea34e2e087c41981a
mouakkid <58151050+mouakkid@users.noreply.github.com>
2026-02-09 12:08:54 +0100

    Initial Commit

---
+++ WiKiRace App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WiKiRace App
+//
+//  Created by Anass Mouakkid on 9/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WiKiRace App/WiKiRace_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiKiRace_AppApp.swift
+//  WiKiRace App
+//
+//  Created by Anass Mouakkid on 9/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct WiKiRace_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
